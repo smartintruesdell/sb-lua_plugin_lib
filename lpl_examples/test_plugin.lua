17430cf9075c5bc7749b031844cca296b20965d5
Shawn Martin-Truesdell <shawn@martin-truesdell.com>
2022-02-18 06:18:00 -0800

    test: test plugin updates

--- lpl_examples/test_plugin.lua
+++ lpl_examples/test_plugin.lua
@@ -12,9 +12,25 @@ inflictedDamageCallback_handle_notification = Plugins.add_before_hook(
   end
 )
 
+inflictedDamageCallback_handle_notification = Plugins.add_before_hook(
+  inflictedDamageCallback_handle_notification,
+  function(notification)
+    sb.logInfo("Plugins: SECOND BEFORE HOOK CALLED")
+    sb.logInfo(util.tableToString(table.pack(notification)))
+    return notification
+  end
+)
+
 inflictedDamageCallback_handle_notification = Plugins.add_after_hook(
   inflictedDamageCallback_handle_notification,
   function()
     sb.logInfo("Plugins: AFTER HOOK CALLED")
+  end
+)
+
+inflictedDamageCallback_handle_notification = Plugins.add_after_hook(
+  inflictedDamageCallback_handle_notification,
+  function()
+    sb.logInfo("Plugins: SECOND AFTER HOOK CALLED")
   end
 )
