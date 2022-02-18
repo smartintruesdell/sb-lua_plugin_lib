3d7958d1456922f37fac3dff5a0df9b179f41bd1
Shawn Martin-Truesdell <shawn@martin-truesdell.com>
2022-02-18 12:52:56 -0800

    fixup! feat(scripts): add plugin support to `items/buildscripts/buildunrandweapon.lua`

--- items/buildscripts/buildunrandweapon.lua
+++ items/buildscripts/buildunrandweapon.lua
@@ -23,23 +23,27 @@ function build(directory, config, parameters, level, seed)
   Plugins.call_before_initialize_hooks("buildunrandweapon")
   -- END PLUGIN LOADER --------------------------------------------------------
 
-  config, parameters = build_set_level(config, parameters, level, seed)
-  config, parameters = build_setup_abilities(config, parameters, seed)
-  config, parameters = build_setup_elemental_type(config, parameters, seed)
-  config, parameters = build_setup_damage_level_multiplier(
-    config,
-    parameters,
-    seed
-  )
-  config, parameters = build_setup_palette_swaps(
-    directory,
-    config,
-    parameters,
-    seed
-  )
-  config, parameters = build_setup_gun_offsets(config, parameters, seed)
-  config, parameters = build_setup_tooltip_fields(config, parameters, seed)
-  config, parameters = build_set_price(config, parameters, seed)
+  config, parameters = build_set_seed(config, parameters, seed)
+  config, parameters = build_set_directory(config, parameters, directory)
+  config, parameters = build_set_level(config, parameters, level)
+  config, parameters = build_set_builderConfig(config,parameters)
+
+  config, parameters = build_setup_abilities(config, parameters)
+  config, parameters = build_setup_elemental_type(config, parameters)
+  config, parameters = build_set_name(config, parameters)
+  config, parameters = build_setup_damage_config(config, parameters)
+  config, parameters = build_setup_shared_primary_attack_config(config,parameters)
+  config, parameters = build_setup_melee_primary_attack_config(config,parameters)
+  config, parameters = build_setup_ranged_primary_attack_config(config,parameters)
+  config, parameters = build_setup_damage_level_multiplier(config,parameters)
+  config, parameters = build_setup_palette_swaps(config,parameters)
+  config, parameters = build_setup_animation_custom(config, parameters)
+  config, parameters = build_setup_animation_parts(config, parameters)
+  config, parameters = build_setup_gun_offsets(config, parameters)
+  config, parameters = build_setup_elemental_fire_sounds(config,parameters)
+  config, parameters = build_setup_inventory_icon(config, parameters)
+  config, parameters = build_setup_tooltip_fields(config, parameters)
+  config, parameters = build_set_price(config, parameters)
 
   -- PLUGIN LOADER ------------------------------------------------------------
   config, parameters = Plugins.call_after_initialize_hooks(
@@ -52,7 +56,25 @@ function build(directory, config, parameters, level, seed)
   return config, parameters
 end
 
-function build_set_level(config, parameters, level, _seed)
+function build_set_seed(config, parameters, seed)
+  -- initialize randomization
+  -- unrandweapon does not apply a random seed
+  if seed then
+    parameters.seed = seed
+  end
+
+  return config, parameters
+end
+
+function build_set_directory(config, parameters, directory)
+  if directory then
+    parameters.directory = directory
+  end
+
+  return config, parameters
+end
+
+function build_set_level(config, parameters, level)
   if
     level and
     not getConfigParameter(
@@ -68,15 +90,45 @@ function build_set_level(config, parameters, level, _seed)
   return config, parameters
 end
 
-function build_setup_abilities(config, parameters, level, _seed)
-  setupAbility(config, parameters, "primary")
-  setupAbility(config, parameters, "alt")
+function build_set_builderConfig(config, parameters)
+  -- unrandweapon has no builderConfig
+  parameters.builderConfig = nil
 
   return config, parameters
 end
 
-function build_setup_elemental_type(config, parameters, _seed)
-  -- elemental type and config (for alt ability)
+function build_setup_abilities(config, parameters)
+  setupAbility(
+    config,
+    parameters,
+    "primary",
+    parameters.builderConfig,
+    parameters.seed
+  )
+  setupAbility(
+    config,
+    parameters,
+    "alt",
+    parameters.builderConfig,
+    parameters.seed
+  )
+
+  return config, parameters
+end
+
+function build_setup_elemental_type(config, parameters)
+  -- elemental type
+  if
+    not parameters.elementalType and
+    type(parameters.builderConfig) == table and
+    parameters.builderConfig.elementalType
+  then
+    parameters.elementalType = randomFromList(
+      parameters.builderConfig.elementalType,
+      parameters.seed,
+      "elementalType"
+    )
+  end
   local elementalType = getConfigParameter(
     config,
     parameters,
@@ -85,12 +137,60 @@ function build_setup_elemental_type(config, parameters, _seed)
   )
   replacePatternInData(config, nil, "<elementalType>", elementalType)
 
+  -- elemental config
+  if
+    type(parameters.builderConfig) == table and
+    parameters.builderConfig.elementalConfig
+  then
+    util.mergeTable(
+      config, parameters.builderConfig.elementalConfig[elementalType]
+    )
+  end
   if config.altAbility and config.altAbility.elementalConfig then
     util.mergeTable(
       config.altAbility,
       config.altAbility.elementalConfig[elementalType]
     )
   end
+
+    -- elemental tag
+  replacePatternInData(config, nil, "<elementalType>", elementalType)
+  replacePatternInData(
+    config,
+    nil,
+    "<elementalName>",
+    elementalType:gsub("^%l", string.upper)
+  )
+
+  return config, parameters
+end
+
+function build_set_name(config, parameters)
+  -- buildunrandweapon doesn't set a name
+
+  return config, parameters
+end
+
+function build_setup_damage_config(config, parameters)
+  -- buildunrandweapon doesn't setup damage config
+
+  return config, parameters
+end
+
+function build_setup_shared_primary_attack_config(config, parameters)
+  -- buildunrandweapon doesn't setup shared attack config
+
+  return config, parameters
+end
+
+function build_setup_melee_primary_attack_config(config, parameters)
+  -- buildunrandweapon doesn't setup melee attack config
+
+  return config, parameters
+end
+
+function build_setup_ranged_primary_attack_config(config, parameters)
+  -- buildunrandweapon doesn't setup ranged attack config
 
   return config, parameters
 end
@@ -111,11 +211,13 @@ function build_setup_damage_level_multiplier(config, parameters, _seed)
   return config, parameters
 end
 
-function build_setup_palette_swaps(directory, config, parameters, _seed)
+function build_setup_palette_swaps(config, parameters)
   -- palette swaps
   config.paletteSwaps = ""
   if config.palette then
-    local palette = root.assetJson(util.absolutePath(directory, config.palette))
+    local palette = root.assetJson(
+      util.absolutePath(parameters.directory, config.palette)
+    )
     local colorIndex = getConfigParameter(
       config,
       parameters,
@@ -145,7 +247,19 @@ function build_setup_palette_swaps(directory, config, parameters, _seed)
   return config, parameters
 end
 
-function build_setup_gun_offsets(config, parameters, seed)
+function build_setup_animation_custom(config, parameters)
+  -- buildunrandweapon doesn't setup animation custom
+
+  return config, parameters
+end
+
+function build_setup_animation_parts(config, parameters)
+  -- buildunrandweapon doesn't setup animation parts
+
+  return config, parameters
+end
+
+function build_setup_gun_offsets(config, parameters)
   if config.baseOffset then
     construct(
       config,
@@ -166,7 +280,19 @@ function build_setup_gun_offsets(config, parameters, seed)
   return config, parameters
 end
 
-function build_setup_tooltip_fields(config, parameters, _seed)
+function build_setup_elemental_fire_sounds(config, parameters)
+  -- buildunrandweapon doesn't setup elemental fire sounds
+
+  return config, parameters
+end
+
+function build_setup_inventory_icon(config, parameters)
+  -- buildunrandweapon doesn't setup an inventory icon
+
+  return config, parameters
+end
+
+function build_setup_tooltip_fields(config, parameters)
   -- populate tooltip fields
   if config.tooltipKind ~= "base" then
     config.tooltipFields = {}
@@ -222,7 +348,7 @@ function build_setup_tooltip_fields(config, parameters, _seed)
   return config, parameters
 end
 
-function build_set_price(config, parameters, _seed)
+function build_set_price(config, parameters)
   -- set price
   -- TODO: should this be handled elsewhere?
   config.price =
