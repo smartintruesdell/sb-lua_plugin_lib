ce4b93933096f45d93038b9b5e224f12c1d7dce6
Shawn Martin-Truesdell <shawn@martin-truesdell.com>
2022-02-19 02:07:55 -0800

    fix(scripts): add missing dependencies to buildfist.lua

--- items/buildscripts/buildfist.lua
+++ items/buildscripts/buildfist.lua
@@ -1,4 +1,7 @@
 require "/scripts/util.lua"
+require "/scripts/vec2.lua"
+require "/scripts/versioningutils.lua"
+require "/scripts/staticrandom.lua"
 
 require "/scripts/lpl_load_plugins.lua"
 require "/scripts/lpl_plugin_util.lua"
