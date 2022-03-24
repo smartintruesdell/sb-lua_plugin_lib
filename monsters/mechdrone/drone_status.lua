6434fd7acbbeb8d9ebc56a18928832867bb63e23
Shawn Martin-Truesdell <shawn@martin-truesdell.com>
2022-03-23 20:36:15 -0700

    feat(scripts): add plugin support to `monsters/mechdrone/**/*.lua`

---
+++ monsters/mechdrone/drone_status.lua
@@ -0,0 +1,51 @@
+require "/scripts/vec2.lua"
+require "/scripts/lpl_load_plugins.lua"
+local PLUGINS_PATH =
+  "/monsters/mechdrone/drone_status_plugins.config"
+
+function init()
+  self.damageFlashTime = 0
+end
+init = PluginLoader.add_plugin_loader("drone_status", PLUGINS_PATH, init)
+
+function applyDamageRequest(damageRequest)
+  if world.getProperty("nonCombat") then
+    return {}
+  end
+
+  local damage = 0
+  if damageRequest.damageType == "Damage" or damageRequest.damageType == "IgnoresDef" then
+    damage = damage + damageRequest.damage
+  else
+    return {}
+  end
+
+  local healthLost = math.min(damage, status.resource("health"))
+  status.modifyResource("health", -healthLost)
+
+  local hitType = damageRequest.hitType
+  if not status.resourcePositive("health") then
+    hitType = "kill"
+  end
+
+  return {{
+    sourceEntityId = damageRequest.sourceEntityId,
+    targetEntityId = entity.id(),
+    position = mcontroller.position(),
+    damageDealt = damage,
+    healthLost = healthLost,
+    hitType = hitType,
+    kind = "Normal",
+    damageSourceKind = damageRequest.damageSourceKind,
+    targetMaterialKind = status.statusProperty("targetMaterialKind")
+  }}
+end
+
+function update(dt)
+  if self.damageFlashTime > 0 then
+    status.setPrimaryDirectives(string.format("fade=%s", "ff0000=0.85"))
+  else
+    status.setPrimaryDirectives()
+  end
+  self.damageFlashTime = math.max(0, self.damageFlashTime - dt)
+end
